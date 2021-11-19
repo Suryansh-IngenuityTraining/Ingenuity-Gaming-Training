acd348c817333f84fe7056ed1dda7c384138d0bc
Suryansh Saxena <suryansh.saxena_cs18@gla.ac.in>
2021-11-19 19:51:49 +0530

    completed

--- CovidApp Assignment7/src/ts/APPService.ts
+++ CovidApp Assignment7/src/ts/APPService.ts
@@ -1,21 +1,23 @@
-import {TCovidData} from './types/CovidData'
+import {TCovidData} from './types/CovidData';
+import {TCovidNews} from './types/CovidNews';
+import {TCovidVacc} from './types/CovidVacc';
 export class APIService{
-    INFO_DATA = "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/";
-    private readonly NEWS_COVID = "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/news/get-coronavirus-news/0";
-    private readonly INFO_VACCINE = "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/vaccines/get-all-vaccines";
-    private readonly headers = new Headers({
+    COV_DATA = "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/";
+    TREATMENT_DATA = "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/vaccines/";
+    VACCINE_DATA = "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/news/";
+    headers = new Headers({
         "x-rapidapi-host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com",
 		"x-rapidapi-key": "12588894bbmshb60963dfbcbb58bp1f9a63jsn14ba8437af08"
 	})
     private readonly method = "GET";
     public getCovidData(): Promise<TCovidData[]>{
-        return this.getData(this.INFO_DATA);
+        return this.getData(this.COV_DATA);
     }
-    public getCovidNews(): Promise<any> {
-        return this.getData(this.NEWS_COVID);
+    public getCovidNews(): Promise<TCovidNews[]> {                          
+        return this.getData(this.TREATMENT_DATA);
     }
-    public getVaccineData(): Promise<any> {
-        return this.getData(this.INFO_VACCINE);
+    public getVaccineData(): Promise<TCovidVacc[]> {
+        return this.getData(this.VACCINE_DATA);
     }
     protected async getData(url: string): Promise<any>{
         const response = await fetch(url, {
